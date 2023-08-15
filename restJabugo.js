c6d1dd72999076e121a0318babd5f681bc1aa2cd
danielerm23 <danielerm006@gmail.com>
2023-08-15 19:32:33 +0200

    version en preparacion, menu bebidas

--- restJabugo.js
+++ restJabugo.js
@@ -1,34 +1,89 @@
 
-let seccionInicio=document.getElementById("seccion-inicio")
-let seccionMesas=document.getElementById("seccion-mesas")
-let seccionProductos=document.getElementById("seccion-productos")
-let mesas=document.getElementById("mesas")
-let divProductos=document.getElementById("div-productos2")
-let botonNumeroMesas=document.getElementById("boton-numero-mesas")
+const seccionInicio=document.getElementById("seccion-inicio")
+const seccionMesas=document.getElementById("seccion-mesas")
+const seccionProductos=document.getElementById("seccion-productos")
+const contenedor=document.getElementById("contenedor")
+
+const divProductos=document.getElementById("div-productos2")
+const divBebidas=document.getElementById("div-bebidas")
+const divRaciones=document.getElementById("div-raciones")
+const divComidas=document.getElementById("div-comidas")
+const mesas=document.getElementById("mesas")
+const resumen=document.getElementById("resumen")
+
+const botonNumeroMesas=document.getElementById("boton-numero-mesas")
+const botonGuardar=document.getElementById("boton-guardar")
+const botonComidas=document.getElementById("boton-comidas")
+const botonRaciones=document.getElementById("boton-raciones")
+const botonBebidas=document.getElementById("boton-bebidas")
+const botonCocaColaOriginal=document.getElementById("boton-coca-cola-original")
+const botonCocaColaZero=document.getElementById("boton-coca-cola-zero")
+const botonCocaColaZeroZero=document.getElementById("boton-coca-cola-zero-zero")
+const botonAguaConGas=document.getElementById("boton-agua-con-gas")
+const botonAguaSinGas=document.getElementById("boton-agua-sin-gas")
+const botonCerveza=document.getElementById("boton-cerveza")
+const botonCervezaSinAlcohol=document.getElementById("boton-cerveza-sin-alcohol")
+const botonNestea=document.getElementById("boton-nestea")
+const botonBebidasMenu=document.querySelectorAll(".boton-bebidas")
+
 const inputElement=document.getElementById("n-mesas")
-let botonGuardar=document.getElementById("boton-guardar")
+
+let objetoMesa=[]
+let registroMesas=[]
+let registroMesasUl
+let elementLi
+let valorBoton
 let mesaSeleccionada 
 let mesaSeleccion
 let mesa
 let intervalo
 
-function iniciarBoton(){
-    seccionMesas.style.display='none'
-    seccionProductos.style.display='none'
-    botonNumeroMesas.addEventListener('click', autorizacion)
-    botonGuardar.addEventListener('click', creacionMesas)
-    
+class objetoMesas{
+    constructor(bebidas, comidas, raciones, total){
+
+        this.bebidas=bebidas
+        this.comidas=comidas
+        this.raciones=raciones
+        this.total=total
+    }
 }
 
+function iniciarBoton(){
+    divBebidas.style.display="none"
+    divRaciones.style.display="none"
+    divComidas.style.display="none"
+    seccionMesas.style.display='none'
+    contenedor.style.display='none'
+
+    botonNumeroMesas.addEventListener('click', autorizacion)
+    botonGuardar.addEventListener('click', creacionMesas)
+    botonComidas.addEventListener('click', comidas)
+    botonRaciones.addEventListener('click', raciones)
+    botonBebidas.addEventListener('click', bebidas)
+    botonBebidasMenu.forEach(boton=> {
+        boton.addEventListener('click', () => {
+            valorBoton=boton.textContent
+            registroPedidos()
+        }) 
+    }); 
+}
 function autorizacion(){
     if (inputElement.value>0 && inputElement.value<101){
+        for (let i = 1; i <= inputElement.value; i++) {
+            objetoMesa.push(new objetoMesas([], [], [], []))
+        }
         creacionMesas()
     }
 }
 function creacionMesas(){
     seccionInicio.style.display='none'
     seccionMesas.style.display='flex'
-    seccionProductos.style.display='none'
+    divBebidas.style.display="none"
+    divRaciones.style.display="none"
+    divComidas.style.display="none"
+    contenedor.style.display='none'
+    resumen.innerHTML=""
+
     let con=1
     let creacionMesas
     while(con<=inputElement.value){
@@ -46,12 +101,12 @@ function creacionMesas(){
 
 function detectarMesaSeleccionada(){
     
-    for (let i = 0; i < inputElement.value; i++) {
-        mesaSeleccionada=document.getElementById(`mesa${i+1}`)
+    for (let i = 1; i <= inputElement.value; i++) {
+        mesaSeleccionada=document.getElementById(`mesa${i}`)
         if (mesaSeleccionada.checked){
             seccionMesas.style.display='none'
-            seccionProductos.style.display='flex'
-            mesa=mesaSeleccionada.id
+            contenedor.style.display='flex'
+            mesa=i
             mesas.innerHTML=""
             funcionProductos ()
         }
@@ -63,5 +118,39 @@ function funcionProductos(){
     mesaSeleccion=`<h4>${mesa}</h4>`
     divProductos.innerHTML=mesaSeleccion
 }
+
+function bebidas() {
+    divBebidas.style.display="flex"
+    divRaciones.style.display="none"
+    divComidas.style.display="none"
+}
+function raciones() {
+    divRaciones.style.display="flex"
+    divBebidas.style.display="none"
+    divComidas.style.display="none"
+}
+function comidas() {
+    divComidas.style.display="flex"
+    divBebidas.style.display="none"
+    divRaciones.style.display="none"
+}
+function registroPedidos(){
+    
+    resumen.innerHTML=""
+    registroMesasUl=document.createElement('ul')
+    objetoMesa[mesa-1].bebidas.forEach(item => {
+        elementLi=document.createElement('li')
+        elementLi.textContent=item
+
+        registroMesasUl.appendChild(elementLi)
+    });
+    resumen.appendChild(registroMesasUl)
+    
+    registroMesas=document.createElement('p')
+    registroMesas.innerHTML=valorBoton
+    objetoMesa[mesa-1].bebidas.push(valorBoton)
+    resumen.appendChild(registroMesas)
+}
+
 
 window.addEventListener('load', iniciarBoton)
